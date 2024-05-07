4b795906fce7587347a683cc2e774684b5589084
Erkan Turker <erkanturker@Erkans-MacBook-Pro.local>
2024-05-06 21:59:20 -0400

    fix link activate

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar>
       <Container fluid className="px-0 m-1">
-        <Navbar.Brand href="/" className="col-auto">
+        <Navbar.Brand as={NavLink} to="/" className="col-auto">
           Jobly
         </Navbar.Brand>
         <Nav className="col-auto">
-          <Nav.Link href="/companies">Companies</Nav.Link>
-          <Nav.Link href="/jobs">Jobs</Nav.Link>
+          <Nav.Link as={NavLink} to="/companies" activeclassname="active">
+            Companies
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/jobs" activeclassname="active">
+            Jobs
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
